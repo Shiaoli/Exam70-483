2daa43391976ae258da0b8f4ef1639aaf6be1ec5
Shiaoli <ntmxl5288@gmial.com>
2018-07-24 11:47:49 -0500

    Hello, GitHub

---
+++ Chapter2/Chapter2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chapter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Little Mao's GitHub travel is begining! Fighting!!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
