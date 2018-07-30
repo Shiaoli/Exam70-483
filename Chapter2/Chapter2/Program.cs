8488c841bafdea3b53d7b0557a6cc456e5bf4ffa
Shiaoli <ntmxl5288@gmial.com>
2018-07-30 16:18:34 -0500

    Tricky situation using new in overriding

--- Chapter2/Chapter2/Program.cs
+++ Chapter2/Chapter2/Program.cs
@@ -9,9 +9,38 @@ namespace Chapter2
         public static int GetSquareOf(int x) => x * x;
         public static double GetPi() => 3.14;
     }
+
+    public class ClassParameters
+    {
+        public int value { get; set; }
+    }
+
+    //overriding base class
+    public class Base
+    {
+        public void Execute() { Console.WriteLine("Base executed!"); }
+    }
+
+    public class Derived : Base
+    {
+        public new void Execute() { Console.WriteLine("Derived executed!"); }
+    }
+
+    public struct StructParameters
+    {
+        public int value { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
+        {
+            Program pro = new Program();
+            //pro.Methods1();
+            pro.Overriding();
+        }
+
+        // Involking methods
+         public void Methods1()
         {
             Console.WriteLine($"Pi is {Math.GetPi()}");
             int x = Math.GetSquareOf(5);
@@ -21,8 +50,22 @@ namespace Chapter2
             Console.WriteLine($"the value of the new Math instance is {math.Value}");
             Console.WriteLine($"the square of the value is {math.GetSquare()}");
             Console.WriteLine($"the static get square is {Math.GetSquareOf(10)}");
-            //Console.WriteLine("Little Mao's GitHub travel is begining! Fighting!!");
-            //Console.WriteLine("Hello World!");
+        }
+
+        // Passing parameters by value
+        public void ClassParametersMethod()
+        {
+            ClassParameters classParameters = new ClassParameters();
+            classParameters.value = 10;
+
+        } 
+
+        public void Overriding()
+        {
+            Base b = new Base();
+            b.Execute();
+            b = new Derived();
+            b.Execute();
         }
     }
 }
